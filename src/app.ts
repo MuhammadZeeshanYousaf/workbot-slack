8dcd84a0639a42913d5fb7bc3c3bbb4134c1b42b
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-10-30 15:36:51 +0500

    CWHA-8464 Remove unnecessary permission scopes.

--- src/app.ts
+++ src/app.ts
@@ -10,16 +10,7 @@ export const app = new App({
   clientId: process.env.SLACK_CLIENT_ID,
   clientSecret: process.env.SLACK_CLIENT_SECRET,
   stateSecret: process.env.SLACK_STATE_SECRET,
-  scopes: [
-    'chat:write',
-    'commands',
-    'users:read',
-    'users:read.email',
-    'im:history',
-    'mpim:history',
-    'channels:read',
-    'channels:history'
-  ],
+  scopes: ['app_mentions:read', 'chat:write', 'commands', 'users:read', 'users:read.email'],
   installerOptions: {
     directInstall: true
   },
