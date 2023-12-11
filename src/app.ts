de4309b89d2ef1928bdbc1d53c61210d90fe38f3
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-12-11 18:39:17 +0500

    Enable processing the ack() before executing the response code.

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ export const database = new Database();
 export const app = new App({
   logLevel: LogLevel.ERROR,
   extendedErrorHandler: true,
+  processBeforeResponse: true,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   clientId: process.env.SLACK_CLIENT_ID,
   clientSecret: process.env.SLACK_CLIENT_SECRET,
