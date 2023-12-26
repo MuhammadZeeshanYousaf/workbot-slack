537918421bd6b6a4b2855fe826ef07d78c17f1a5
Muhammad Zeeshan Yousaf <zeeshanyousaf430@gmail.com>
2023-12-26 19:00:04 +0500

    Remove old `deprecated` companies drop-down.

--- src/slack/blocks/index.ts
+++ src/slack/blocks/index.ts
@@ -1,6 +1,5 @@
 export * from './basic.block';
 export * from './mention.block';
-export * from './companies.block';
 export * from './unlink-company.block';
 export * from './link-company.block';
 export * from './welcome.block';
